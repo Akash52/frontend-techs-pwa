6277fb5addb68156cb82d30e3c6cfc634e6e4f77
Akash52 <ac8572611@gmail.com>
2023-05-25 03:34:33 +0530

    meta data added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,11 +6,17 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="theme-color" content="#ffffff" />
+          <meta name="description" content="NextJS Rental PWA" />
+          <meta name="keywords" content="NextJS Rental PWA" />
+          <meta name="author" content="NextJS Rental PWA" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
           <link
             rel="apple-touch-icon"
             href="/icons/apple-touch-icon.png"
           ></link>
+          <title>NextJS Rental PWA</title>
         </Head>
         <body>
           <Main />
