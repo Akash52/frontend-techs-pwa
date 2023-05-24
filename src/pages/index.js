558dda9c6264c529f25457379fb15d3e42277fe2
Akash52 <ac8572611@gmail.com>
2023-05-25 03:20:55 +0530

    next rental starter code

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import RentalListings from "@/components/RentalListings";
+import React from "react";
+
+const index = () => {
+  return (
+    <>
+      <RentalListings />
+    </>
+  );
+};
+
+export default index;
